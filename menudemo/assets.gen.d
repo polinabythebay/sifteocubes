3ba7c48ea72dd7b667c6ea9c62d0dfdf5aa886bb
Polina Soshnin <psoshnin@wellesley.edu>
2013-10-31 13:54:16 -0400

    added code
    added code

---
+++ menudemo/assets.gen.d
@@ -0,0 +1,55 @@
+assets.gen.o: assets.gen.cpp \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/asset.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/asset/group.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/abi.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/syscall.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/types.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/audio.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/events.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/vram.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/asset.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/elf.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/limits.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/macros.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/math.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/asset/loader.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/array.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/memory.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/asset/audio.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/asset/image.h
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/asset.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/asset/group.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/abi.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/syscall.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/types.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/audio.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/events.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/vram.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/asset.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/elf.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/limits.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/macros.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/math.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/asset/loader.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/array.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/memory.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/asset/audio.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/asset/image.h:
---
+++ menudemo/main.d
@@ -0,0 +1,132 @@
+main.o: main.cpp /Users/Goddess/Desktop/sifteocubes/include/sifteo.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/abi.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/syscall.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/types.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/audio.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/events.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/vram.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/asset.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/elf.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/array.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/memory.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/macros.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/asset.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/asset/group.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/limits.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/math.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/asset/loader.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/asset/audio.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/asset/image.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/audio.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/cube.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/event.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/filesystem.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/metadata.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/motion.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/string.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/system.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/time.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/video.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/video/color.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/video/sprite.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/video/framebuffer.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/video/bg0rom.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/video/bg0.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/video/bg1.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/video/bg2.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/video/tilebuffer.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/menu.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/menu/types.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/menu/util.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/menu/public.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/menu/states.h \
+  /Users/Goddess/Desktop/sifteocubes/include/sifteo/menu/events.h \
+  assets.gen.h
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/abi.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/syscall.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/types.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/audio.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/events.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/vram.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/asset.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/abi/elf.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/array.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/memory.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/macros.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/asset.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/asset/group.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/limits.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/math.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/asset/loader.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/asset/audio.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/asset/image.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/audio.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/cube.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/event.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/filesystem.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/metadata.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/motion.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/string.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/system.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/time.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/video.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/video/color.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/video/sprite.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/video/framebuffer.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/video/bg0rom.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/video/bg0.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/video/bg1.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/video/bg2.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/video/tilebuffer.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/menu.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/menu/types.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/menu/util.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/menu/public.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/menu/states.h:
+
+/Users/Goddess/Desktop/sifteocubes/include/sifteo/menu/events.h:
+
+assets.gen.h:
