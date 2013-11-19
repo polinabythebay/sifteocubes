c93cf4b104322144d0373e7aba4aa3a325bdce62
Spyra <danika.suggs@gmail.com>
2013-11-19 01:18:17 -0500

    Working Implementation of Setting Tasks
    Tilt and then click to set a task.
    Click a task to set it to yellow.
    Click again to set it to blue.
    Click once more to go back to the main menu.

---
+++ task3/assets.gen.d
@@ -0,0 +1,54 @@
+assets.gen.o: assets.gen.cpp /Users/danika/sifteo/include/sifteo/asset.h \
+  /Users/danika/sifteo/include/sifteo/asset/group.h \
+  /Users/danika/sifteo/include/sifteo/abi.h \
+  /Users/danika/sifteo/include/sifteo/abi/syscall.h \
+  /Users/danika/sifteo/include/sifteo/abi/types.h \
+  /Users/danika/sifteo/include/sifteo/abi/audio.h \
+  /Users/danika/sifteo/include/sifteo/abi/events.h \
+  /Users/danika/sifteo/include/sifteo/abi/vram.h \
+  /Users/danika/sifteo/include/sifteo/abi/asset.h \
+  /Users/danika/sifteo/include/sifteo/abi/elf.h \
+  /Users/danika/sifteo/include/sifteo/limits.h \
+  /Users/danika/sifteo/include/sifteo/macros.h \
+  /Users/danika/sifteo/include/sifteo/math.h \
+  /Users/danika/sifteo/include/sifteo/asset/loader.h \
+  /Users/danika/sifteo/include/sifteo/array.h \
+  /Users/danika/sifteo/include/sifteo/memory.h \
+  /Users/danika/sifteo/include/sifteo/asset/audio.h \
+  /Users/danika/sifteo/include/sifteo/asset/image.h
+
+/Users/danika/sifteo/include/sifteo/asset.h:
+
+/Users/danika/sifteo/include/sifteo/asset/group.h:
+
+/Users/danika/sifteo/include/sifteo/abi.h:
+
+/Users/danika/sifteo/include/sifteo/abi/syscall.h:
+
+/Users/danika/sifteo/include/sifteo/abi/types.h:
+
+/Users/danika/sifteo/include/sifteo/abi/audio.h:
+
+/Users/danika/sifteo/include/sifteo/abi/events.h:
+
+/Users/danika/sifteo/include/sifteo/abi/vram.h:
+
+/Users/danika/sifteo/include/sifteo/abi/asset.h:
+
+/Users/danika/sifteo/include/sifteo/abi/elf.h:
+
+/Users/danika/sifteo/include/sifteo/limits.h:
+
+/Users/danika/sifteo/include/sifteo/macros.h:
+
+/Users/danika/sifteo/include/sifteo/math.h:
+
+/Users/danika/sifteo/include/sifteo/asset/loader.h:
+
+/Users/danika/sifteo/include/sifteo/array.h:
+
+/Users/danika/sifteo/include/sifteo/memory.h:
+
+/Users/danika/sifteo/include/sifteo/asset/audio.h:
+
+/Users/danika/sifteo/include/sifteo/asset/image.h:
