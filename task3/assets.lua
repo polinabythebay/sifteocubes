20211f627705762fe5f59b489bde1b444db99ae2
scusack14 <sydcusack@gmail.com>
2013-12-05 14:20:03 -0500

    New Images Added, lua updated

--- task3/assets.lua
+++ task3/assets.lua
@@ -13,17 +13,20 @@ StripeTile = image{ "assets/stripes.png", pinned=1 }
 Task1_Icon = image{"assets/task1icon.png"}
 Task2_Icon = image{"assets/task2icon.png"}
 Task3_Icon = image{"assets/task3icon.png"}
+Task4_Icon = image{"assets/task4icon.png"}
+Task5_Icon = image{"assets/task5icon.png"}
+Task6_Icon = image{"assets/task6icon.png"}
 
 Footer = image{ "assets/Footer.png" }
 LabelEmpty = image{ "assets/LabelEmpty.png" }
 
 -- Task Assets
 
-TaskReds = image{{"assets/task1red.png", "assets/task2red.png", "assets/task3red.png"}}
-TaskYellows = image{{"assets/task1yellow.png", "assets/task2yellow.png", "assets/task3yellow.png"}}
-TaskBlues = image{{"assets/task1blue.png", "assets/task2blue.png", "assets/task3blue.png"}}
-TaskPics = image{{"assets/task1Pic.png", "assets/task2Pic.png", "assets/task3Pic.png"}}
-TaskDurations = image{{"assets/task1Duration.png", "assets/task2Duration.png", "assets/task3Duration.png"}}
+TaskReds = image{{"assets/task1red.png", "assets/task2red.png", "assets/task3red.png", "assets/task4red.png", "assets/task5red.png", "assets/task6red.png"}}
+TaskYellows = image{{"assets/task1yellow.png", "assets/task2yellow.png", "assets/task3yellow.png", "assets/task4yellow.png", "assets/task5yellow.png", "assets/task6yellow.png"}}
+TaskBlues = image{{"assets/task1blue.png", "assets/task2blue.png", "assets/task3blue.png", "assets/task4blue.png", "assets/task5blue.png", "assets/task6blue.png"}}
+TaskPics = image{{"assets/task1Pic.png", "assets/task2Pic.png", "assets/task3Pic.png", "assets/task4Pic.png", "assets/task5Pic.png", "assets/task6Pic.png"}}
+TaskDurations = image{{"assets/task1Duration.png", "assets/task2Duration.png", "assets/task3Duration.png", "assets/task4Duration.png", "assets/task5Duration.png", "assets/task6Duration.png"}}
 
 -- Connection Assets
 
