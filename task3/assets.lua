62d46bb78ff00ca459247858467e588dc103147b
Spyra <danika.suggs@gmail.com>
2013-12-15 13:53:59 -0500

    fixed?

---
+++ task3/assets.lua
@@ -0,0 +1,33 @@
+-- Metadata
+
+IconAssets = group{quality=9.95}
+Icon = image{"assets/icon.png"}
+
+-- Menu Assets
+
+BootstrapAssets = group{quality=10}
+
+BgTile = image{ "assets/bg.png", pinned=1 }
+StripeTile = image{ "assets/stripes.png", pinned=1 }
+
+Task1_Icon = image{"assets/task1icon.png"}
+Task2_Icon = image{"assets/task2icon.png"}
+Task3_Icon = image{"assets/task3icon.png"}
+
+Footer = image{ "assets/Footer.png" }
+LabelEmpty = image{ "assets/LabelEmpty.png" }
+
+-- Task Assets
+
+TaskReds = image{{"assets/task1red.png", "assets/task2red.png", "assets/task3red.png"}}
+TaskYellows = image{{"assets/task1yellow.png", "assets/task2yellow.png", "assets/task3yellow.png"}}
+TaskBlues = image{{"assets/task1blue.png", "assets/task2blue.png", "assets/task3blue.png"}}
+
+-- Connection Assets
+
+Bars = {
+	image{ "assets/bar_top.png", pinned=true },
+	image{ "assets/bar_left.png", pinned=true },
+	image{ "assets/bar_bottom.png", pinned=true },
+	image{ "assets/bar_right.png", pinned=true }
+}
