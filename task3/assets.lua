7861245f67052e0afc7403b9ad637dc865decd77
Polina Soshnin <psoshnin@wellesley.edu>
2013-12-16 15:02:24 -0500

    merge
    merge

--- task3/assets.lua
+++ task3/assets.lua
@@ -5,7 +5,11 @@ Icon = image{"assets/icon.png"}
 
 -- Menu Assets
 
+<<<<<<< HEAD
 BootstrapAssets = group{quality=10}
+=======
+BootstrapAssets = group{quality=8.90}
+>>>>>>> scusack_branch
 
 BgTile = image{ "assets/bg.png", pinned=1 }
 StripeTile = image{ "assets/stripes.png", pinned=1 }
@@ -13,12 +17,19 @@ StripeTile = image{ "assets/stripes.png", pinned=1 }
 Task1_Icon = image{"assets/task1icon.png"}
 Task2_Icon = image{"assets/task2icon.png"}
 Task3_Icon = image{"assets/task3icon.png"}
+<<<<<<< HEAD
+=======
+Task4_Icon = image{"assets/task4icon.png"}
+Task5_Icon = image{"assets/task5icon.png"}
+Task6_Icon = image{"assets/task6icon.png"}
+>>>>>>> scusack_branch
 
 Footer = image{ "assets/Footer.png" }
 LabelEmpty = image{ "assets/LabelEmpty.png" }
 
 -- Task Assets
 
+<<<<<<< HEAD
 TaskReds = image{{"assets/task1red.png", "assets/task2red.png", "assets/task3red.png"}}
 TaskYellows = image{{"assets/task1yellow.png", "assets/task2yellow.png", "assets/task3yellow.png"}}
 TaskBlues = image{{"assets/task1blue.png", "assets/task2blue.png", "assets/task3blue.png"}}
@@ -31,3 +42,59 @@ Bars = {
 	image{ "assets/bar_bottom.png", pinned=true },
 	image{ "assets/bar_right.png", pinned=true }
 }
+=======
+TaskReds = image{{"assets/task1red.png", "assets/task2red.png", "assets/task3red.png", "assets/task4red.png", "assets/task5red.png", "assets/task6red.png"}}
+TaskYellows = image{{"assets/task1yellow.png", "assets/task2yellow.png", "assets/task3yellow.png", "assets/task4yellow.png", "assets/task5yellow.png", "assets/task6yellow.png"}}
+TaskBlues = image{{"assets/task1blue.png", "assets/task2blue.png", "assets/task3blue.png", "assets/task4blue.png", "assets/task5blue.png", "assets/task6blue.png"}}
+TaskPics = image{{"assets/task1Pic.png", "assets/task2Pic.png", "assets/task3Pic.png", "assets/task4Pic.png", "assets/task5Pic.png", "assets/task6Pic.png"}}
+TaskDurations = image{{"assets/task1Duration.png", "assets/task2Duration.png", "assets/task3Duration.png", "assets/task4Duration.png", "assets/task5Duration.png", "assets/task6Duration.png"}}
+
+
+-- Connection Assets
+
+RedBars = {
+	image{ "assets/rbar_vert.png", pinned=true },
+	image{ "assets/rbar_hor.png", pinned=true },
+	image{ "assets/rbar_vert.png", pinned=true },
+	image{ "assets/rbar_hor.png", pinned=true }
+}
+
+OrangeBars = {
+	image{ "assets/obar_vert.png", pinned=true },
+	image{ "assets/obar_hor.png", pinned=true },
+	image{ "assets/obar_vert.png", pinned=true },
+	image{ "assets/obar_hor.png", pinned=true }
+}
+
+YellowBars = {
+	image{ "assets/ybar_vert.png", pinned=true },
+	image{ "assets/ybar_hor.png", pinned=true },
+	image{ "assets/ybar_vert.png", pinned=true },
+	image{ "assets/ybar_hor.png", pinned=true }
+}
+
+GreenBars = {
+	image{ "assets/gbar_vert.png", pinned=true },
+	image{ "assets/gbar_hor.png", pinned=true },
+	image{ "assets/gbar_vert.png", pinned=true },
+	image{ "assets/gbar_hor.png", pinned=true }
+}
+
+BlueBars = {
+	image{ "assets/bbar_vert.png", pinned=true },
+	image{ "assets/bbar_hor.png", pinned=true },
+	image{ "assets/bbar_vert.png", pinned=true },
+	image{ "assets/bbar_hor.png", pinned=true }
+}
+
+PurpleBars = {
+	image{ "assets/pbar_vert.png", pinned=true },
+	image{ "assets/pbar_hor.png", pinned=true },
+	image{ "assets/pbar_vert.png", pinned=true },
+	image{ "assets/pbar_hor.png", pinned=true }
+}
+
+-- Animation Assets
+
+PaperAnimation = image{"assets/paperanimation/paperjoined.png", pinned=true, width=128, height=128}
+>>>>>>> scusack_branch
